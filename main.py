7dacf413d6a961a7b8181c11ccc7960d33283484
priyankaudupa <149789558+priyankaudupa@users.noreply.github.com>
2023-11-03 17:54:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ return {
 
 def test_bucketing_by_health():
   print("Counting batteries by SoH...\n")
-  present_capacities = [113, 116, 80, 95, 92, 70]
+  present_capacities = [20,85,50,90,110,120,100,30,49,119,102,99]
   counts = count_batteries_by_health(present_capacities)
   assert(counts["healthy"] == 2)
   assert(counts["exchange"] == 3)
